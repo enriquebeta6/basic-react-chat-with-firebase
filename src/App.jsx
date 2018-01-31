424282d2c33513ec3c2469beb5480c436cf5844c
enriquebeta7 <enriquebeta7@gmail.com>
2018-01-31 21:08:35 -0200

    Basic React Chat with Firebase

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import ChatRoom from './components/ChatRoom';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <ChatRoom />
+    )
+  }
+}     
